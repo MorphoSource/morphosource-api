ef04f2ce0e78e5541dd3d69962f20a9f9830d761
Julie Winchester <julie.winchester@duke.edu>
2023-06-26 13:17:02 -0500

    Modified 18 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjEzNDUyNA/spectral.js?branch_slug=master&token=eebeba4b-0ef8-4223-9b71-7c22316ad856";
+export default { extends: ruleset };
